3ea1751e7382dea545243fd3104ffa6db2eaca50
juliajykim <jkim24@gwu.edu>
2021-03-19 15:45:22 -0400

    Add anagram file

--- anagrams.rb
+++ anagrams.rb
@@ -1,6 +1,16 @@
-anagram?("gizmo", "sally")    #=> false
-anagram?("elvis", "lives")    #=> true
+# canagram?("gizmo", "sally")    #=> false
+# anagram?("elvis", "lives")    #=> true
 
 def first_anagram?(str1, str2)
-    
+  possible = str1.split("").permutation.to_a #O(n!)
+  possible.map! {|ele| ele.join("")} #O(n)
+  possible.include?(str2) #O(n)
 end
+
+
+def second_anagram?(str1, str2)
+
+end
+
+# p first_anagram?("gizmo", "sally")
+# find_index and delete
